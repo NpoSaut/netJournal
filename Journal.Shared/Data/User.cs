058782064af3d4e85af5f0ba0a034b41b72933f4
Evgeniy Plyusnin <eninbox@gmail.com>
2014-11-12 11:01:57 +0500

    Изменена сущность базы данных User: добавлены поля для имени, фамилии и отчества

--- Journal.Shared/Data/User.cs
+++ Journal.Shared/Data/User.cs
@@ -1,19 +1,41 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Journal.Data
 {
+    /// <summary>Пользователь</summary>
     public class User
     {
         public User() { Sessions = new HashSet<Session>(); }
 
-        public User(string Name) : this() { this.Name = Name; }
+        public User(string LoginName, string Name, string Surname, string Patronymic = null) : this()
+        {
+            this.LoginName = LoginName;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Patronymic = Patronymic;
+        }
 
+        /// <summary>Идентификатор пользователя</summary>
         public int Id { get; set; }
 
+        /// <summary>AD-Логин</summary>
         [Required]
-        public string Name { get; set; }
+        public string LoginName { get; set; }
 
+        /// <summary>Имя</summary>
+        [Required]
+        public String Name { get; set; }
+
+        /// <summary>Фамилия</summary>
+        [Required]
+        public String Surname { get; set; }
+
+        /// <summary>Отчество</summary>
+        public String Patronymic { get; set; }
+
+        /// <summary>Список сессий</summary>
         public virtual ICollection<Session> Sessions { get; set; }
     }
 }
