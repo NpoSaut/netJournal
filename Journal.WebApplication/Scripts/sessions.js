5af21ae85a3ded5e3ea8b9f3a0345d772fe59675
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2014-11-13 09:56:36 +0500

    В комментарии накидан черновик по работе с сессиями

---
+++ Journal.WebApplication/Scripts/sessions.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    //var sessionRunning;
+
+    //$.getJSON("Api/IsSessionRunning?userid=user", function (data) {
+    //    sessionRunning = data.running;
+    //});
+
+    //$("#checkinButton").click(function () {
+    //    var userTime = $(".time-editor").
+
+    //    if (sessionRunning) {
+    //        $.getJSON("Api/CloseSession?userid=user&startTime=time", function (data) {
+    //            if (data.success)
+    //                sessionRunning = false;
+    //        });
+    //    } else {
+    //        $.getJSON("Api/OpenSession?userid=user&startTime=time", function (data) {
+    //            if (data.success)
+    //                sessionRunning = true;
+    //        });
+    //    }
+    //});
+
+    //sessionRunning.onchange = function () {
+
+    //};
+
+});
